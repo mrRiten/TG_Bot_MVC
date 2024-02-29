1f9651686e5f75f30923c97a397c095e17466682
Igroman4ik <ni4000009@gmail.com>
2024-02-29 20:17:57 +0300

    Fix

--- Parser/Program.cs
+++ Parser/Program.cs
@@ -43,7 +43,7 @@ namespace Parser
             {
                 Console.WriteLine($"Произошла ошибка: {ex.Message}");
                 Thread.Sleep(1800000);
-                Main([]);
+                Main(args);
             }
         }
 
